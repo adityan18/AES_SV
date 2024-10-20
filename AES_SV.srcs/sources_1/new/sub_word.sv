5965c688a4f5fd5395bc9caefd7c059f3217bd4f
adityan18 <aditya.sushma2001@gmail.com>
2024-10-20 19:21:31 +0530

    Added sbox and inv sbox

--- AES_SV.srcs/sources_1/new/sub_word.sv
+++ AES_SV.srcs/sources_1/new/sub_word.sv
@@ -22,11 +22,12 @@
 
 module sub_word(
     input [31:0] in_word, /* Input Word */
+    input enc_or_dec, /* Encryption or Decryption */
     output [31:0] out_word /* Output Sub Word */
     );
 
-    sbox s1(.in_byte(in_word[31-: 8]), .sub_byte(out_word[31 -: 8]));
-    sbox s2(.in_byte(in_word[23-: 8]), .sub_byte(out_word[23 -: 8]));
-    sbox s3(.in_byte(in_word[15-: 8]), .sub_byte(out_word[15 -: 8]));
-    sbox s4(.in_byte(in_word[7 : 0]), .sub_byte(out_word[7 : 0]));
+    sbox s1(.in_byte(in_word[31-: 8]), .sub_byte(out_word[31 -: 8]), .enc_or_dec(enc_or_dec));
+    sbox s2(.in_byte(in_word[23-: 8]), .sub_byte(out_word[23 -: 8]), .enc_or_dec(enc_or_dec));
+    sbox s3(.in_byte(in_word[15-: 8]), .sub_byte(out_word[15 -: 8]), .enc_or_dec(enc_or_dec));
+    sbox s4(.in_byte(in_word[7 : 0]), .sub_byte(out_word[7 : 0]), .enc_or_dec(enc_or_dec));
 endmodule
