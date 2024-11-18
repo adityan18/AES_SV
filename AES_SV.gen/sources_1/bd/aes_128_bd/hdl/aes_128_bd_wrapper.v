1137ca246aa6274d1b0dd082a146a4ccdfb9ff82
adityan18 <aditya.sushma2001@gmail.com>
2024-11-18 22:32:08 +0530

    Added block design

---
+++ AES_SV.gen/sources_1/bd/aes_128_bd/hdl/aes_128_bd_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+//Date        : Mon Nov 18 22:24:37 2024
+//Host        : Aditya running 64-bit major release  (build 9200)
+//Command     : generate_target aes_128_bd_wrapper.bd
+//Design      : aes_128_bd_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module aes_128_bd_wrapper
+   ();
+
+
+  aes_128_bd aes_128_bd_i
+       ();
+endmodule
